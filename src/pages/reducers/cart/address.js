c5971a8da8405aa70259e96e6897d611f3c36f47
zyxpz <mythmxlcool@vip.qq.com>
2018-04-21 23:12:34 +0800

    add: remove

--- src/pages/reducers/cart/address.js
+++ src/pages/reducers/cart/address.js
@@ -1,8 +1,19 @@
 import * as types from '../../constants/actions/cart';
 const initialState = {
-	list: []
 };
+
+const delet = params => {
+	const {
+		id,
+		list,
+	} = params;
+
+	let arr = list.filter(val => val.id !== id);
+	return arr;
+};
+
 export default function address(state = initialState, action = {}) {
+	let id, data;
 	switch (action.type) {
 		case types.CART_ADDRESS_MAIN_GET + '_ON':
 			state = {
@@ -13,6 +24,14 @@ export default function address(state = initialState, action = {}) {
 			state = {
 				...state,
 				list: action.data.data,
+			};
+			return state;
+		case types.CART_ADDRESS_DEL_MAIN_GET + '_SUCCESS':
+			id = action.param.id;
+			data = delet({ id, list: action.data.data });
+			state = {
+				...state,
+				list: data,
 			};
 			return state;
 		default:
